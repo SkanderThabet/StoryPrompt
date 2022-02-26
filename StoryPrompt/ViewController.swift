d60aec17b6c6636aeaa68af04157b17c7abed79d
SkanderThabet <skander.thabet@esprit.tn>
2022-02-26 16:03:47 +0100

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Skander Thabet on 26/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
