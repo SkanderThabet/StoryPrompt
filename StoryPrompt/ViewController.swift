951496d81e11691e6e9c10af24049227a4e38b4a
SkanderThabet <skander.thabet@esprit.tn>
2022-02-26 18:06:56 +0100

    added photo picker

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
     
@@ -16,10 +17,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var nounTextField: UITextField!
     @IBOutlet weak var adjectiveTextField: UITextField!
     @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var storyPromptImageView: UIImageView!
     //Slider
     @IBOutlet weak var numberSlider: UISlider!
     //Number label
     @IBOutlet weak var numberLB: UILabel!
+    
+    //Button
+    @IBAction func generateStoryPrompt(_ sender: Any) {
+        updateStoryPrompt()
+        print(storyPrompt)
+        
+    }
+    
+    
     
     
     //Event actions
@@ -42,10 +53,55 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        numberSlider.value=7.5
-        storyPrompt.number=(Int(numberSlider.value))
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = Int(numberSlider.value)
+        storyPromptImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        storyPromptImageView.addGestureRecognizer(gestureRecognizer)
     }
+    func updateStoryPrompt()  {
+        storyPrompt.noun = nounTextField.text ?? ""
+        storyPrompt.adjective = adjectiveTextField.text ?? ""
+        storyPrompt.verb = verbTextField.text ?? ""
+    }
+    @objc func changeImage() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        let controller = PHPickerViewController(configuration: configuration)
+        controller.delegate = self
+        present(controller, animated: true)
+      }
+}
 
 
+
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    updateStoryPrompt()
+    return true
+  }
+}
+
+extension ViewController: PHPickerViewControllerDelegate {
+  func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+    if !results.isEmpty {
+      let result = results.first!
+      let itemProvider = result.itemProvider
+      if itemProvider.canLoadObject(ofClass: UIImage.self) {
+        itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+          guard let image = image as? UIImage else {
+            return
+          }
+          DispatchQueue.main.async {
+            self?.storyPromptImageView.image = image
+          }
+        }
+      }
+    }
+  }
 }
 
