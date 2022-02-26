068cb2fb7c9c9f4091c8476c4d5e6353f7988c0f
SkanderThabet <skander.thabet@esprit.tn>
2022-02-26 17:04:44 +0100

    added switch genre type via controller

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Properties
+    let storyPrompt = StoryPromptEntry()
+    
+    //TextFields
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    //Slider
+    @IBOutlet weak var numberSlider: UISlider!
+    //Number label
+    @IBOutlet weak var numberLB: UILabel!
+    
+    
+    //Event actions
+    @IBAction func changedNumber(_ sender: UISlider) {
+        numberLB.text="Number : \(Int(sender.value))"
+        storyPrompt.number=(Int(sender.value))
+    }
+    @IBAction func changedStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex){
+            storyPrompt.genre = genre
+        }
+        else {
+            storyPrompt.genre = .scifi
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        numberSlider.value=7.5
+        storyPrompt.number=(Int(numberSlider.value))
     }
 
 
