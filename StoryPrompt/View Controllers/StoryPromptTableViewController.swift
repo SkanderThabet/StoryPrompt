ebe99780f9a29ae3903aa2267904d7d6087627d2
SkanderThabet <skander.thabet@esprit.tn>
2022-03-02 00:34:58 +0100

    Added Table View Controller as a root controller

---
+++ StoryPrompt/View Controllers/StoryPromptTableViewController.swift
@@ -0,0 +1,135 @@
+//
+//  StoryPromptTableViewController.swift
+//  StoryPrompt
+//
+//  Created by Skander Thabet on 2/3/2022.
+//
+
+import UIKit
+
+class StoryPromptTableViewController: UITableViewController {
+   
+    
+    var storyPrompts = [StoryPromptEntry]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storyPrompt1 = StoryPromptEntry()
+        let storyPrompt2 = StoryPromptEntry()
+        let storyPrompt3 = StoryPromptEntry()
+        
+        storyPrompt1.noun = "toaster"
+        storyPrompt1.verb = "attacks"
+        storyPrompt1.adjective = "smelly"
+        storyPrompt1.number = 5
+        
+        storyPrompt2.noun = "toaster"
+        storyPrompt2.verb = "attacks"
+        storyPrompt2.adjective = "smelly"
+        storyPrompt2.number = 5
+        
+        storyPrompt3.noun = "toaster"
+        storyPrompt3.verb = "attacks"
+        storyPrompt3.adjective = "smelly"
+        storyPrompt3.number = 5
+        
+        storyPrompts = [storyPrompt1,storyPrompt2,storyPrompt3]
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return storyPrompts.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryPromptCell", for: indexPath)
+        cell.textLabel?.text = "Prompt \(indexPath.row + 1)"
+        cell .imageView?.image = storyPrompts[indexPath.row].image
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyPrompt = storyPrompts[indexPath.row]
+        performSegue(withIdentifier: "ShowStoryPrompt", sender: storyPrompt)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowStoryPrompt" {
+            guard let storyPromptViewController = segue.destination as? StoryPromptViewController,
+                  let storyPrompt = sender as? StoryPromptEntry else {
+                      return
+                  }
+            storyPromptViewController.storyPrompt = storyPrompt
+        }
+    }
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
